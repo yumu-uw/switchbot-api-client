592bc0bb2ea76fb4d1176fc486162331a57caedb
yumuuw <yumuuw@gmail.com>
2024-01-06 20:18:37 +0900

    influxdbへの出力を実装

--- main.go
+++ main.go
@@ -1,31 +1,47 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-
+	"github.com/joho/godotenv"
+	"github.com/yumu-uw/switchbot-api-client/exporter"
 	"github.com/yumu-uw/switchbot-api-client/util"
 )
 
+func loadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("can't read .env file")
+	}
+}
+
 func main() {
-	o := make(map[string][]interface{})
+	loadEnv()
+	util.InitApiUtil()
+
+	var status_list []map[string]interface{}
 	device_list := util.GetDeviceList()
 	for _, d := range device_list {
-		switch d.DeviceType {
-		case "Plug Mini (JP)", "Plug Mini (US)":
-			r := util.GetPlugMiniStatus(d.DeviceID)
-			r.Device.DeviceName = d.DeviceName
-			o[d.DeviceType] = append(o[d.DeviceType], r)
-		case "Hub 2":
-			r := util.GetHub2Status(d.DeviceID)
-			r.Device.DeviceName = d.DeviceName
-			o[d.DeviceType] = append(o[d.DeviceType], r)
+		r := util.GetDeviceStatus(d.DeviceID)
+		r["DeviceName"] = d.DeviceName
+		r["DeviceType"] = d.DeviceType
+		status_list = append(status_list, r)
+	}
+
+	for _, v := range status_list {
+		device_type, _ := v["DeviceType"].(string)
+		device_name, _ := v["DeviceName"].(string)
+		body := v["body"].(map[string]interface{})
+
+		tags := map[string]string{
+			"device_type": device_type,
+			"device_name": device_name,
 		}
+		exporter.InitInfluxDBExporter()
+		exporter.ExportToInfluxDB(tags, body)
 	}
-	bytes, err := json.Marshal(o)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(string(bytes))
+	// bytes, err := json.Marshal(o)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// fmt.Println(string(bytes))
+	// util.DBTest()
 }
